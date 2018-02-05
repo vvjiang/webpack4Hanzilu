6b3f5483840ee11d2511e436b998e23f418933e2
韩子卢 <vvjiang@Foxmail.com>
2018-02-06 00:33:14 +0800

    引入jest

--- test/app.test.js
+++ test/app.test.js
@@ -1,11 +1,5 @@
-import {assert} from 'chai'
 import addNum from '../src/js/addNum'
 
-describe('测试index.js',()=> {
-  describe('测试addNum函数', ()=> {
-    it('两数相加结果为两个数字的和', ()=> {
-      assert.equal(addNum(1,2),3)
-    })
-  })
-})
-
+test('两数相加结果为两个数字的和', () => {
+    expect(addNum(1, 2)).toBe(3);
+});
--- test/container.test.js
+++ test/container.test.js
@@ -1,23 +1,20 @@
-import {assert} from 'chai'
 import React from 'react'
 import Enzyme from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import Container from '../src/components/container'
 
-const {render,mount}=Enzyme
+const {render,mount} = Enzyme
 
-Enzyme.configure({ adapter: new Adapter() })
+Enzyme.configure({
+    adapter: new Adapter()
+})
 
-describe('测试Container组件之render', () => {
-  it('Container组件包含Header组件', () => {
+test('Container组件包含Header组件', () => {
     let app = render(<Container />)
-    assert.equal(app.find("span.header-title").length,1)
-  });
-});
+    expect(app.find("span.header-title").length).toBe(1)
+})
 
-describe('测试Container组件之mount', () => {
-  it('Container组件包含Header组件', () => {
-    let app = mount(<Container />)
-    assert.equal(app.find("span.header-title").length,1)
-  });
+test('Container组件包含Header组件', () => {
+    let app = mount( <Container />)
+    expect(app.find("span.header-title").length).toBe(1)
 });
