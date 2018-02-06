55155da50e6ff04c07112771f775cbec51fde0c8
韩子卢 <vvjiang@foxmail.com>
2018-02-06 13:47:40 +0800

    更多玩法

--- test/header.test.js
+++ test/header.test.js
@@ -7,47 +7,46 @@ import Header from '../src/components/header'
 const { shallow } = Enzyme
 
 Enzyme.configure({ adapter: new Adapter() })
+describe("测试123", () => {
+    test('Header组件中各位置的文本符合预期输入', function () {
+        const title = '当前主题',
+            leftTitle = '左边主题',
+            rightTitle = '右边主题',
+            leftClick = jest.fn()
 
-test('Header组件中各位置的文本符合预期输入', function () {
-    const title = '当前主题',
-        leftTitle = '左边主题',
-        rightTitle = '右边主题',
-        leftClick = sinon.spy()
+        let app = shallow(< Header title={title} leftTitle={leftTitle} rightTitle={rightTitle} onLeftClick={leftClick} />);
+        app.find('span.header-left-title').simulate("click")
 
-    let app = shallow(< Header title={title} leftTitle={leftTitle} rightTitle={rightTitle} onLeftClick={leftClick} />);
-    app.find('span.header-left-title').simulate("click")
+        expect(leftClick).toBeCalled()
+        expect(leftClick.mock.calls.length).toBe(1);//函数只被调用一次
 
-    expect(leftClick.calledOnce)
-
-    expect(app.find('span.header-left-title').text()).toBe(leftTitle);
-    expect(app.find('span.header-right-title').text()).toBe(rightTitle);
-    expect(app.find('span.header-title').text()).toBe(title);
+        expect(app.find('span.header-left-title').text()).toBe(leftTitle);
+        expect(app.find('span.header-right-title').text()).toBe(rightTitle);
+        expect(app.find('span.header-title').text()).toBe(title);
+    })
 })
+
 
 test('Header组件中左侧文本点击有效', function () {
     const title = '当前主题',
         leftTitle = '左边主题',
         rightTitle = '右边主题',
-        leftClick = sinon.spy()
+        leftClick = jest.fn()
 
     let app = shallow(< Header title={title} leftTitle={leftTitle} rightTitle={rightTitle} onLeftClick={leftClick} />)
     app.find('span.header-left-title').simulate("click")
-    expect(leftClick.calledOnce)
+    expect(leftClick).toBeCalled()
 })
 
 test('对原有函数的stub封装，可以监听原有函数的调用情况,以及模拟返回', function () {
     const obj = {
-        func: () => {
-            console.info(1)
-        }
+        func: jest.fn()
     }
     const num = 42
+    obj.func.mockReturnValue(num)
+    const result = obj.func(3)
 
-    sinon.stub(obj, 'func').returns(num)
-
-    const result = obj.func(3);
-
-    expect(obj.func.calledOnce)
-    expect(obj.func.getCall(0).args[0]).toBe(3)
+    expect(obj.func).toBeCalled()
+    expect(obj.func.mock.calls[0][0]).toBe(3)//这个函数被第一次调用时的第一个 arg 是 3
     expect(result).toBe(num)
 });
