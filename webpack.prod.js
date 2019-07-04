ef0a9c896e50fc26ec92588195c4bb24924bcbe5
韩子卢 <vvjiang@foxmail.com>
2019-07-04 19:16:54 +0800

    MiniCssExtractPlugin取代ExtractTextPlugin，splitChunks取代CommonsChunkPlugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,57 +1,42 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const webpack = require('webpack');
 
 module.exports = merge(common, {
   entry: {
-    vendor: [
-      'react',
-      'react-dom',
-      'redux',
-      'react-router-dom',
-      'react-redux',
-      'redux-actions',
-      'axios'
-    ],
   },
   plugins: [
     new BundleAnalyzerPlugin({ analyzerPort: 8919 }),
-    new webpack.optimize.CommonsChunkPlugin({
-      names: ['vendor'],
-      minChunks: Infinity,
-      filename: 'common.bundle.[chunkhash].js',
-    }),
-    new webpack.optimize.CommonsChunkPlugin({
-      names: ['manifest'],
-      filename: 'manifest.bundle.[chunkhash].js',
-    }),
-    new ExtractTextPlugin({ filename: '[name].[contenthash].css', allChunks: false }),
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash:8].css", chunkFilename: "[id].css" })
   ],
+  optimization: {
+    //取代 CommonsChunkPlugin,抽取公共代码
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          name: "commons",
+          chunks: "initial",
+          minChunks: 2
+        }
+      }
+    }
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader?modules'],
-        }),
+        use: [MiniCssExtractPlugin.loader, 'css-loader?modules'],
       }, {
         test: /\.css$/,
         include: /node_modules/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader']
-        }),
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.less$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader?modules', 'less-loader']
-        }),
+        use: [MiniCssExtractPlugin.loader, 'css-loader?modules', 'less-loader']
       },
     ],
   }
