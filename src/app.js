82c459db75640a203206c2f84a6c70a13def705c
韩子卢 <vvjiang@foxmail.com>
2019-07-04 18:41:50 +0800

    用React.lazy和Suspense取代react-loadable

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
@@ -6,8 +6,7 @@ import createLogger from 'redux-logger';
 import promiseMiddleware from 'redux-promise';
 import ReactDOM from 'react-dom';
 // import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import Loadable from 'react-loadable';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import reducer from './reducers';
 import './app.less';
 
@@ -15,20 +14,9 @@ function Loading() {
   return <div>Loading...</div>;
 }
 
-const PageMain = Loadable({
-  loader: () => import('./components/pageMain'),
-  loading: Loading,
-});
-
-const PageSearch = Loadable({
-  loader: () => import('./components/pageSearch'),
-  loading: Loading,
-});
-
-const PageReader = Loadable({
-  loader: () => import('./components/pageReader'),
-  loading: Loading,
-});
+const PageMain = React.lazy(() => import('./components/pageMain'));
+const PageSearch = React.lazy(() => import('./components/pageSearch'));
+const PageReader = React.lazy(() => import('./components/pageReader'));
 
 
 const store = createStore(reducer, applyMiddleware(thunk, createLogger, promiseMiddleware));
@@ -36,11 +24,13 @@ const store = createStore(reducer, applyMiddleware(thunk, createLogger, promiseM
 const App = () => (
   <Provider store={store}>
     <Router>
-      <div>
-        <Route exact path="/" component={PageMain} />
-        <Route path="/search" component={PageSearch} />
-        <Route path="/reader/:bookid/:link" component={PageReader} />
-      </div>
+      <Suspense fallback={<Loading />}>
+        <Switch>
+          <Route exact path="/" component={PageMain} />
+          <Route path="/search" component={PageSearch} />
+          <Route path="/reader/:bookid/:link" component={PageReader} />
+        </Switch>
+      </Suspense>
     </Router>
   </Provider>
 );
