f80d1dccce1ed4b925fa4731a1028bcc3901cbc4
韩子卢 <vvjiang@foxmail.com>
2019-12-23 19:09:18 +0800

    OpenLayer的安装和使用

--- src/modules/pageWebGIS/index.tsx
+++ src/modules/pageWebGIS/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Empty } from 'antd'
 import { connect } from 'dva';
+import { Map, View } from 'ol';
+import { fromLonLat } from 'ol/proj';
+import { Tile } from 'ol/layer';
+import { XYZ } from 'ol/source';
 import styles from './index.css'
 
 interface DataItems {
@@ -28,14 +31,31 @@ const mapStateToProps = ({ fundModel }) => {
  */
 @connect(mapStateToProps)
 export default class PageMain extends React.Component<IPageMainProps> {
+  map = {}
   componentDidMount() {
-
+    this.map = new Map({
+      target: 'map',
+      layers: [
+        new Tile({
+          source: new XYZ({
+            url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+          })
+        })
+      ],
+      view: new View({
+        center: fromLonLat([114.30, 30.60]),
+        zoom: 8
+      })
+    });
   }
+
+
 
   render() {
     return (
-      <div>
-        webgis
+      <div className={styles.container}>
+        <div className={styles.title}>WebGIS示例</div>
+        <div id="map" className={styles.map} ></div>
       </div>
     );
   }
