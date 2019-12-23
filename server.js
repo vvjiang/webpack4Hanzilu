570d11b77e44ed3a7544bb5090b9b5a4143978bb
韩子卢 <vvjiang@foxmail.com>
2019-12-23 12:58:54 +0800

    爬虫爬取js文件上的基金信息

--- server.js
+++ server.js
@@ -37,26 +37,26 @@ app.get('/getList', (rootreq, rootres) => {
 });
 
 //  获取所有基金的信息（包括名字和基金代码）
-app.get('/getFunds', (rootres) => {
+app.get('/getFunds', (rootreq, rootres) => {
   rootres.header('Access-Control-Allow-Origin', '*');
   rootres.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
   rootres.header('Access-Control-Allow-Headers', 'X-Requested-With');
   rootres.header('Access-Control-Allow-Headers', 'Content-Type');
 
-  const reptileUrl = `http://fund.eastmoney.com/js/fundcode_search.js`;
+  const reptileUrl = 'http://fund.eastmoney.com/js/fundcode_search.js';
 
   superagent.get(reptileUrl)
     .set('Accept', '*/*')
     .set('Accept-Language', 'zh-CN,zh;q=0.9')
     .set('Accept-Encoding', 'gzip, deflate')
     .set('Pragma', 'no-cache')
+    .set('Cache-Control', 'no-cache')
     .set('Referer', 'http://fundf10.eastmoney.com/jjjz_100032.html')
     .set('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36')
     .set('Cookie', 'qgqp_b_id=c635d33b4d446a13f46a66eea6872c5c; EMFUND1=null; EMFUND2=null; EMFUND3=null; EMFUND4=null; EMFUND5=null; EMFUND6=null; EMFUND7=null; EMFUND8=null; EMFUND0=null; EMFUND9=11-06 10:30:03@#$%u5BCC%u56FD%u4E2D%u8BC1%u7EA2%u5229%u6307%u6570%u589E%u5F3A@%23%24100032; st_si=55988030695388; st_pvi=85185825910210; st_sp=2019-09-30%2009%3A41%3A05; st_inirUrl=https%3A%2F%2Fwww.baidu.com%2Flink; st_sn=1; st_psi=20191106103003311-0-1320550964; st_asi=delete')
-    .end((err) => {
-      let returnText = err.rawResponse.replace(`${callbackFucStr}(`, '');
-      returnText = returnText.substr(0, returnText.length - 1);
-      rootres.send(returnText);
+    .buffer(true)
+    .end((err, res) => {
+      rootres.send(res.text.replace('var r = ', '').replace(';', ''));
     });
 });
 
